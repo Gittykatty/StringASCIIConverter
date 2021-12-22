1caae3694292485744dcc6d4722d63c82e37b828
Gittykatty <88427749+Gittykatty@users.noreply.github.com>
2021-12-23 00:18:46 +0100

    Clean up Code

--- converter.py
+++ converter.py
@@ -6,11 +6,10 @@ for character in text:
     ascii_values.append(ord(character))
 hex_values = [hex(x) for x in ascii_values]
 
-print("Text: ", text)
-print("Dec: ", ascii_values)
-print("Hex: ", hex_values)
-
 splitedSize = 4
 hex_splited = [hex_values[x:x+splitedSize] for x in range(0, len(hex_values), splitedSize)]
 
+print("Text: ", text)
+print("Dec: ", ascii_values)
+print("Hex: ", hex_values)
 print("Splitted into 4 Byte: ", hex_splited)
